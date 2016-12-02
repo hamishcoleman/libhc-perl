52b7e59481d9699873061c0de12032efb9a6857b
Hamish Coleman <hamish@zot.org>
2016-12-02 09:14:43 +0800

    Add a more object oriented curses listbox thingy

---
+++ Curses/UI/Listbox/Object.pm
@@ -0,0 +1,73 @@
+package HC::Curses::UI::Listbox::Object;
+use warnings;
+use strict;
+#
+# A version of Curses::UI::Listbox that is slightly more object oriented than
+# the original
+#
+
+use base qw(Curses::UI::Listbox);
+
+sub new() {
+    my $class = shift;
+    my %userargs = @_;
+
+    return undef if (!defined($userargs{-rendervalue}));
+
+    if (!defined($userargs{-renderlabel})) {
+        $userargs{-renderlabel} = \&DefaultRenderLabel;
+    }
+
+    my $self = $class->SUPER::new( %userargs );
+    return undef if (!defined($self));
+
+    $self->labels($self->RenderLabels());
+
+    $self->set_routine('option-select', \&view_object);
+
+    # todo
+    # - mouseclick moves selection, but does not activate it
+    # - doubleclock can still activate
+
+    return $self;
+}
+
+# By default, we simply stringify the object, which might result in a
+# pleasing label anyway ...
+sub DefaultRenderLabel($) {
+    my $value = shift;
+    return ''. $value;
+}
+
+sub RenderLabels() {
+    my $self = shift;
+
+    my $labels;
+    foreach (@{$self->{-values}}) {
+        $labels->{$_} = &{$self->{-renderlabel}}($_);
+    }
+    return $labels;
+}
+
+sub view_object() {
+    my $listbox = shift;
+
+#    # set the highlighted object to be the selected one
+#    $listbox->{-selected} = $listbox->{-ypos};
+#    # Get the selected object
+#    my $object = $listbox->get;
+#    $this->{-selected} = undef;
+
+    # TODO:
+    #my @objects = $listbox->get();
+    #for each @objects, rendervalue
+
+    my $object = $listbox->get_active_value();
+    &{$listbox->{-rendervalue}}($listbox,$object);
+}
+
+# requery / refresh
+# layout / resize?
+
+1;
+
